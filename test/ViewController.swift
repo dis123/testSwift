7e16cb94f6bcfa809c0b7788e314746389e5536f
Dishant <AIG@Dishant.local>
2016-09-30 14:15:43 +0530

    First Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Dishant on 9/30/16.
+//  Copyright © 2016 dishant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let username = "Hello World"
+        print(username)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
